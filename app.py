ce9165e9aba0c4944a2d48959669991da779905d
ivan <kaliuzhny2000@gmail.com>
2022-12-24 20:08:51 +0300

    database connected with postgresql and sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from dotenv import load_dotenv, dotenv_values
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv('.flaskenv')
+
+dbConfig = {
+    **dotenv_values('.env.database'),
+}
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = dbConfig['SQLALCHEMY_DATABASE_URI']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
